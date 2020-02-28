14588041169b768f793ac3d7089873a4bff66abe
ifiamawood <821371350@qq.com>
2020-02-28 15:08:56 +0800

    11

--- lianxi3/lianxi3.cpp
+++ lianxi3/lianxi3.cpp
@@ -1,11 +1,31 @@
 // lianxi3.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
 
+#include "stdafx.h"
+#include<opencv.hpp>
+
+using namespace cv;
 
 int main()
 {
-    return 0;
+	Mat srcMat = imread("D:\\1.jpg");
+	int  height = srcMat.rows;
+	int width = srcMat.cols;
+	uchar threshold = 100;
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold) average = 255;
+			else average = 0;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
 }
 
